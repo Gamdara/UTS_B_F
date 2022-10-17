83bfbcac7a6a2cdd2b4c7ba42a99e375d289faa1
Frederikus Mardiyanto W <94357450+frederikusmardiyantow@users.noreply.github.com>
2022-10-17 15:14:09 +0700

    +isi dashboard user,admin

--- views/pages/admin/dashboard.php
+++ views/pages/admin/dashboard.php
@@ -12,13 +12,13 @@
                     <div class="card">
                         <div class="card-body">
                             <?php if($_SESSION['user']['role']=="admin") {?>
-                                <h2 style="font-weight:700; text-align:center" class="animate__animated animate__bounceInDown mt-4 mb-4">Anda Masuk Sebagai <a href="<?= url() ?>/pages/user/editProfilePage.php" style="text-decoration:none"><?= $data['nama']?></a></h2>
+                                <h2 style="font-weight:700; text-align:center" class="animate__animated animate__bounceInDown mt-4 mb-4">Anda Masuk Sebagai <a href="<?= url() ?>/views/pages/user/editProfilePage.php" style="text-decoration:none"><?= $data['nama']?></a></h2>
                             
                             <?php }else {?>
                             <div class="mt-1 animate__animated animate__zoomIn" style="text-align:center">
                                 <img src="<?= url() ?>/assets/img/image-hello.gif" alt="PerpusKita Logo" style="">
                             </div>
-                            <h2 style="font-weight:800; text-align:center" class="animate__animated animate__bounceInDown">SELAMAT DATANG <br><a href="<?= url() ?>/pages/user/editProfilePage.php" style="text-decoration:none"><?= $data['nama']?></a>  di</h2>
+                            <h2 style="font-weight:800; text-align:center" class="animate__animated animate__bounceInDown">SELAMAT DATANG <br><a href="<?= url() ?>/views/pages/user/editProfilePage.php" style="text-decoration:none"><?= $data['nama']?></a>  di</h2>
                             <div class="animate__animated animate__fadeInUp mt-3 mb-3 gambar-dashboard" style="text-align:center">
                                 <img src="<?= url() ?>/assets/img/PerpusKita.png" alt="PerpusKita Logo" style="">
                             </div>
