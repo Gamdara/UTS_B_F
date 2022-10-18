61c1500878cbdd163ac076f45a25582391483102
Neydeline <dneydeline@gmail.com>
2022-10-18 18:22:22 +0700

    db buku

--- tubes_pw_f.sql
+++ tubes_pw_f.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 16, 2022 at 06:19 PM
+-- Generation Time: Oct 18, 2022 at 01:20 PM
 -- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- PHP Version: 8.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,12 @@ CREATE TABLE `bukus` (
 --
 
 INSERT INTO `bukus` (`id`, `id_genre`, `id_penulis`, `judul`, `jumlah`, `sinopsis`, `cover`) VALUES
-(4, 11, 1, 'qjf', 30, 'wde', '634a665cad00c.png');
+(8, 19, 5, 'Harry Poter', 145, 'Diselamatkan dari bibi & paman yang terlalu mengabaikannya, seorang bocah dengan garis takdir besar membuktikan kemampuannya saat ia mulai memasuki Sekolah Sihir Hogwarts.', '634e88ea1d750.jpg'),
+(9, 21, 6, 'Kancil', 67, 'Kancil yang cerdik', '634e896383be8.jpg'),
+(10, 12, 7, 'Cantik Itu Luka', 90, 'mengisahkan tentang seorang perempuan keturunan Belanda bernama Dewi Ayu yang memiliki paras sangat cantik. Namun, kecantikannya tersebut bukannya membawa sesuatu yang menguntungkan, melainkan malah membawa malapetaka dan kutukan bagi dirinya, beserta keturunannya.', '634e89b625dbd.jpg'),
+(11, 20, 8, 'Cerita-cerita Bahagia Hampir Seluruhnya', 150, 'cerita dari para karakter yang sedang berjuang untuk menjadi dirinya sendiri, namun terhalang oleh dunia heteronormatif yang ada di tengah masyarakat, yang akhirnya membuat mereka harus menerima segala konsekuensi tidak mengenakkan dari lingkungan sekitarnya.', '634e8a02a3438.jpg'),
+(12, 1, 9, ' The Star and I ', 16, 'eorang aktris bernama Olivia Mitchell. Ia bekerja di sebuah teater yang berada di New York. Selain itu, ia ingin sekali mengetahui keberadaan orang tua kandungnya di New York. Namun, mencari seseorang di besarnya kota megapolitan ini tentunya tak mudah.', '634e8a47298d3.jpg'),
+(13, 12, 10, 'Layangan Putus', 6, 'menceritakan tentang kehidupan rumah tangga dari pasangan suami istri bernama Aris dan Kinan yang tiba-tiba diterpa oleh masalah orang ketiga, disaat rumah tangga mereka sebelumnya sangat harmonis.', '634e8a79b0fcc.jpg');
 
 -- --------------------------------------------------------
 
@@ -62,7 +67,10 @@ CREATE TABLE `genres` (
 INSERT INTO `genres` (`id`, `genre`) VALUES
 (1, 'FIksi'),
 (11, 'Thriller'),
-(12, 'Drama');
+(12, 'Drama'),
+(19, 'Action'),
+(20, 'Romantis'),
+(21, 'Dongeng');
 
 -- --------------------------------------------------------
 
@@ -78,14 +86,6 @@ CREATE TABLE `peminjamans` (
   `tanggal_kembali` date NOT NULL,
   `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `peminjamans`
---
-
-INSERT INTO `peminjamans` (`id`, `id_buku`, `id_user`, `tanggal_pinjam`, `tanggal_kembali`, `status`) VALUES
-(4, 4, 5, '2022-10-16', '2022-10-23', 0),
-(5, 4, 5, '2022-10-16', '2022-10-23', 0);
 
 -- --------------------------------------------------------
 
@@ -106,7 +106,12 @@ CREATE TABLE `penulis` (
 --
 
 INSERT INTO `penulis` (`id`, `nama`, `asal`, `tl`, `foto`) VALUES
-(1, 'qwe', 'qwe', '2022-10-06', '');
+(5, 'J.K.Rowling', 'Inggris', '1965-07-31', '634e8b091e81c.jpg'),
+(6, 'Rahimidin Zahari', 'Indonesia', '1968-03-28', '634e870fe27e7.jpg'),
+(7, 'Eka Kurniawan', 'Indonesia', '2020-01-23', '634e8ab0b2343.jpg'),
+(8, 'Norman Erikson Pasaribu', 'Jakarta', '2016-11-21', '634e8b45b2b63.jpg'),
+(9, 'Ilana Tan', 'New York', '1998-10-08', '634e8ae26186d.jpg'),
+(10, 'Mommy ASF', 'London', '1997-05-15', '634e8b2a6d39a.jpg');
 
 -- --------------------------------------------------------
 
@@ -129,10 +134,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `nama`, `email`, `password`, `foto`, `role`) VALUES
 (1, 'nama', 'email.com', 'd6e1c05c8a81c2ae74c7aedea5ec92c1', '', 'admin'),
-(3, 'qwe', 'qw', '006d2143154327a64d86a264aea225f3', '633d256599e93.png', 'user'),
-(4, 'qwe', 'qwe', '$2y$10$am6vfvu3kdn4iuMpB7JW/.fhGUL0ivaVylxA4HcZ9y4dKId0owCku', 'Screenshot 2021-10-20 143558.png', 'user'),
-(5, 'gde rama vedanta yudhistira', 'asd', '$2y$10$BQCfumrOYa1ttT2o2b4eZ.UDm5mqpzSxj7DaRCcnineBvML0NvV4W', 'Screenshot 2021-10-20 143558.png', 'user'),
-(6, 'admin', 'admin', '$2y$10$ty8JzNnvd8d4kGu5Ct/h8uWQQEeGHMJ19ITzeMmw.XTz2EeNZ9NBq', 'Screenshot 2021-10-20 143558.png', 'admin');
+(6, 'admin', 'admin', '$2y$10$ty8JzNnvd8d4kGu5Ct/h8uWQQEeGHMJ19ITzeMmw.XTz2EeNZ9NBq', 'Screenshot 2021-10-20 143558.png', 'admin'),
+(8, 'NEYDELINE DORMA', 'dneydeline@gmail.com', '$2y$10$sr0OwH5jjTifNQOfaV..POppeisbFIYL00zKVTNuTp9qGTbMzDBSi', 'tiket.jpg', 'user');
 
 --
 -- Indexes for dumped tables
@@ -180,31 +183,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bukus`
 --
 ALTER TABLE `bukus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `peminjamans`
 --
 ALTER TABLE `peminjamans`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `penulis`
 --
 ALTER TABLE `penulis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
